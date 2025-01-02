4a42743c3bdb4ec374c605f610c6fe41dfa42ef5
Spencer Sheehan <35203741+spencercsheehan1@users.noreply.github.com>
2025-01-02 15:40:43 -0800

    Update matcha-sip.py

--- matcha-sip.py
+++ matcha-sip.py
@@ -1,49 +1,5 @@
 # FILEPATH: matcha-sip.py
 
-"""
-matcha-sip.py
-Description:
-    -This script fetches all merged pull requests from the specified repositories and filters them by the date range specified in the script.
-    -Recommended to sip matcha while running this script.
-    
-    The script then writes the pull requests to a text file in the following format:
-        -Pull request number, pull request title, pull request url, merged_at date, and author username.
-
-    The script creates a text file for each repository in a specified directory and also prints the pull request number to the console as it is being written to the file.
-
-Usage:
-    Run the script using the following command: python matcha-sip.py
-
-Requirements:
-    - Homebrew
-    - Python 3,x
-
-API Token:
-    The script requires a GitHub API token to access the GitHub API.
-    Please run this script with a token in the GITHUB_TOKEN environment variable.
-
-Environment Variables:
-    Set the environment variables prior to running the script each time per audit, Here's an example:
-    export GITHUB_TOKEN={token_value}
-    export START_DATE={YYYY-MM-DD}
-    export END_DATE={YYYY-MM-DD}
-    export GITHUB_REPOS={list of repos}
-    OUTPUT DIR = os-environ {[Output Directory]}
-
-Date Range:
-    The script filters pull requests by the date range specified in the start date and end date variables.
-    Please update these variables to the desired date range - you can ask auditors what this should be.
-
-Repositories:
-    The script fetches pull requests from the repositories specified in the REPOS List.
-    Please update this list to include the desired repositories. Confirm with auditors what this should be.
-
-Output:
-    The output location is what you specifiy (see environment variables section above).
-    The text file for each repository contains the pull requests that were merged within the specified date range.
-    Yes, we know text files are annoying but this is the easiest way to get the data into the spreadsheet. We can work on a better solution later! :)
-"""
-
 import requests
 import json
 import os
